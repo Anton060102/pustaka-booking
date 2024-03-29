a1a0b96e7cb09bbcfed7ce2b370bb52f6a7379a0
Anton060102 <anton060102@gmail.com>
2024-03-29 22:34:15 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Muhammad Anton
+    Saya tinggal di daerah Cengkareng
+    olahraga yang saya sukai adalah
+    Bulutangkis";
+}
+}
